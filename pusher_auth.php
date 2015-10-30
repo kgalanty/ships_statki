1ac1fe8cba507d5cbe3e5fe5fd5ec89e6844d37d
Krzysztof Galanty <dataz.karas@gmail.com>
2015-10-31 00:08:51 +0100

    Kanały Websocket
    zmiany w obsłudze kanałów

---
+++ pusher_auth.php
@@ -0,0 +1,14 @@
+<?php 
+require('Pusher.php');
+
+$app_id = '142419';
+$app_key = '875a535db554ca357be9';
+$app_secret = 'd97dd660cfe8f4e8a900';
+
+$pusher = new Pusher($app_key, $app_secret, $app_id);
+list(,$userId)=explode('.', $_POST['socket_id']);
+$auth = $pusher->socket_auth($_POST['channel_name'], $userId);
+ //$presence_data = array('user_id' => $userId);
+ $presence_data = array();
+  echo $pusher->presence_auth($_POST['channel_name'], $_POST['socket_id'], $userId, $presence_data);
+  ?>
